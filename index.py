1876fcc2207f79ebde2a5144ea5f8ac24306a365
rashotech <rashotech@gmail.com>
2024-08-01 00:36:18 +0100

    update

--- index.py
+++ index.py
@@ -1,29 +1,25 @@
+import os
+import deepl
 import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
+translator = deepl.Translator(DEEPL_API_KEY)
+
+def translatetext(text, source_lang, target_lang,formality="prefer_more"):
+    result = translator.translate_text(text,source_lang=source_lang,target_lang=target_lang, formality=formality)
+    return result.text
 
 # Prompt the user for input values
 input_text = input("Enter the text to be translated: ")
 source_language = input("Enter the source language: ")
 target_language = input("Enter the target language: ")
-formality = input("Enter the formality level (prefer_more/prefer_less): ")
+formality = input("Enter the formality level (formal/casual): ")
 
 lang = {"english": "EN", "spanish": "ES", "french": "FR", "german": "DE"}
 formal = {"formal": "prefer_more", "casual": "prefer_less"}
 
-# Prepare the data payload for the POST request
-payload = {
-    'input_text': input_text,
-    'source_languages': lang[source_language],
-    'target_languages': lang[target_language],
-    'formality': formal[formality]
-}
-
-# Make the POST request to the translation endpoint
-response = requests.post('http://127.0.0.1:5000/translate/terminal', data=payload)
-
-# Print the response from the server
-if response.status_code == 200:
-    print("Translation successful!")
-    print("Response HTML content:")
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
+print(translatetext(input_text, lang[source_language], lang[target_language], formal[formality]))
